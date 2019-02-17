393999129d4e224b21de9811c9342675e29d6da5
dhyana1984 <xiongyi_1984@foxmail.com>
2019-02-18 00:14:34 +0800

    添加侦听器，实现刷新页面后加载数据

--- mark down notebook/script.js
+++ mark down notebook/script.js
@@ -2,7 +2,10 @@ new Vue({
     el:"#notebook",
     data(){
         return {
-            content:"this is a note."
+            //第一次加载页面时加载数据
+            content: localStorage.getItem("content") || "You can write in **markdown**"
+            //content:"this is a note."
+        
         }
     },
     computed: {
@@ -13,21 +16,38 @@ new Vue({
     },
     //修改侦听器
     watch: {
-        // content:{
-        //     handler(val,oldval){
-        //         console.log("new note: ",val," old note: ",oldval)
-        //     }
-        // },
-        //不需要deep或immediate时的常用语法
-        content(val, oldval){
-            console.log("new note: ",val," old note: ",oldval)
-            localStorage.setItem("content",val)
+        content:{
+            handler:"saveNote"
         },
+        //不需要deep或immediate时的常用语法
+        // content(val, oldval){
+        //     console.log("new note: ",val," old note: ",oldval)
+        //     localStorage.setItem("content",val)
+        // },
         //立即触发调用处理函数
         // immediate:true
     },
+    methods: {
+        saveNote(val){
+            console.log("saving note: ", val)
+            localStorage.setItem("content", val)
+            //methods内部通过this关键字访问Vue实例
+            this.reportOperation("saving")
+        },
+        reportOperation(opName){
+            console.log("The ",opName," operation was completed!")
+        }
+    },
+    //当实例准备就绪就会调用这个钩子
+    //第一次加载页面时获取数据
+    // created() {
+    //     //将contene设置为存储的内容
+    //     //如果没有保存任何内容则设置一个默认字符串
+    //     this.content = localStorage.getItem("content") || "You can write in **markdown**"
+    // },
 })
 
+console.log("restored note: ", localStorage.getItem("content"))
 // const html = marked("**bold** *Italic* [link](http://vuewjs.org)")
 // console.log(html)
 //console输出<p><strong>bold</strong> <em>Italic</em> <a href="http://vuewjs.org">link</a></p>
