be095571afaa20abe1781ff88cf52c691fb03f67
dhyana1984 <xiongyi_1984@foxmail.com>
2019-03-17 23:18:26 +0800

    删除config文件

--- server-GeoBlog/src/config.js
+++
@@ -1,8 +0,0 @@
-export const PORT = process.env.PORT || 3000
-export const SECRET = process.env.SECRET || 'TR7_9cDZ5Re-@lT3Z1|58F'
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:8080'
-export const PUBLIC_URL = process.env.PUBLIC_URL || 'http://localhost:3000'
-export const GOOGLE_CLIENT_ID = "356822051437-06kjh4nd6s35v83p9kao4458lfr9rv8m.apps.googleusercontent.com"
-export const GOOGLE_CLIENT_SECRET = "VvKRu0FvILblEYPM5zXX8ijA"
-export const DB_PATH = process.env.DB_PATH || './db'
-export const COMPACTION_INTERVAL = process.env.COMPACTION_INTERVAL || 12 * 60 * 60 * 1000
